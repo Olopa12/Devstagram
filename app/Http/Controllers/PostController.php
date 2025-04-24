eea835053f977355197e9adb3d64edd9448ef047
Olopa12 <paoventmar@gmail.com>
2025-04-24 17:27:23 +0200

    Primer commit de Devstagram

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(User $user)
+    {
+        $posts =Post::where('user_id', $user->id)->paginate(20);
+
+        return view('dashboard', [
+            'user' => $user,
+            'posts' => $posts
+        ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required|max:255',
+            'descripcion' => 'required',
+            'imagen' => 'required'
+        ]);
+
+        /*Post::Create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);*/
+
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect()->route('post.index', auth()->user()->username);
+    }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,    
+            'user' => $user
+        ]);
+    }
+
+    public function destroy(Post $post)
+    {
+        if($post->user_id === auth()->user()->id) {
+            dd('Si es la misma');
+        } else{
+            dd('Ni es la misma');
+        }
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ComentarioController;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+
+Route::get('/', function () {
+    return view('principal');
+});
+
+Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
+Route::post('/crear-cuenta', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login');
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('post.create')->middleware('auth');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
+Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->
+name('comentarios.store');
+
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
